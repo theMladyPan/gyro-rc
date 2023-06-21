a3bef1da7ba9e8537d0fb2d3b5991f649987fd2d
theMladyPan <stanislav@rubint.sk>
2023-06-21 21:45:15 +0200

    working IBUS and Display

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,45 @@
 #include <Arduino.h>
 #include <IBusBM.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+
+#include <TFT_eSPI.h> 
+#include <SPI.h>
 
 IBusBM IBus;    // IBus object
 int vals[6];    // array to hold channel values
 int loopnr = 0; // loop counter
+
+#define AA_FONT_SMALL "NotoSansBold15"
+#define AA_FONT_LARGE "NotoSansBold36"
+
+#ifndef TFT_DISPOFF
+#define TFT_DISPOFF 0x28
+#endif
+
+#ifndef TFT_SLPIN
+#define TFT_SLPIN   0x10
+#endif
+
+#define ADC_EN          14
+#define ADC_PIN         34
+#define BUTTON_1        35
+#define BUTTON_2        0
+
+TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
+
+static const char *TAG = "main";
+
+std::stringstream ss;
+
+//! Long time delay, it is recommended to use shallow sleep, which can effectively reduce the current consumption
+void espDelay(int ms) //use-> espDelay(6000);
+{   
+    esp_sleep_enable_timer_wakeup(ms * 1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH,ESP_PD_OPTION_ON);
+    esp_light_sleep_start();
+}
 
 void setup() {
     // put your setup code here, to run once:
@@ -17,6 +52,11 @@ void setup() {
 
     // adding 2 sensors
     IBus.addSensor(IBUSS_INTV);
+
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK, true);
 }
 
 void loop() {
@@ -26,14 +66,22 @@ void loop() {
     for (int i=0; i<6; i++) {
         vals[i] = IBus.readChannel(i);  // read channel i, value is between 1000 and 2000
         // print channel values
+        /*
         Serial.print("Channel ");
         Serial.print(i+1);
         Serial.print(": ");
         Serial.print(vals[i]);
         Serial.print("\t");
+        tft.drawString(String(vals[i]), 10, 10, 2);
+        */
+        ss.str("");
+        ss << "Channel " << i+1 << ": " << vals[i] << "\t";
+        tft.drawString(ss.str().c_str(), 10, i*16, 2);
+        Serial.print(ss.str().c_str());
     }
-    Serial.println("\n");
+    Serial.println();
     IBus.setSensorMeasurement(0, loopnr); // set sensor value to 1234
         
-    delay(100);
+    espDelay(100);
 }
+
